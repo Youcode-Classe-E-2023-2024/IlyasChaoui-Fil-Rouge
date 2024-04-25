4fe18bf070fd19b371a009bca8a36e3518ab7d72
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-04-25 10:17:10 +0100

    adding all routes nedded

--- gym-tracker-app/routes/web.php
+++ gym-tracker-app/routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgetPasswrodController;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\frontOffice\homeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\backOffice\ClasseController;
+use App\Http\Controllers\backOffice\PricingController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\ClasseController;
-use App\Http\Controllers\homeController;
-use App\Http\Controllers\PricingController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\backOffice\BenefitsController;
+use App\Http\Controllers\backOffice\CategoryController;
+use App\Http\Controllers\backOffice\DashboardController;
+use App\Http\Controllers\frontOffice\BlogsController;
+use App\Http\Controllers\frontOffice\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,19 +30,21 @@ use App\Http\Controllers\PricingController;
 
 Route::get('/', [homeController::class , 'index'])->name('home');
 
-Route::get('/pricing', [PricingController::class,'index'])->name('show.pricing');
+Route::get('/pricing', [HomeController::class, 'showPricing'])->name('show.pricing');
 
-Route::get('/serviceDetails', function () {
-    return view('homePages.serviceDetails');
-});
-Route::get('/classes', [ClasseController::class ,'index'])->name('show.classes');
-Route::get('/coaches', [UserController::class, 'showCoaches'])->name('show.coaches');
-Route::get('/bim', function () {
-    return view('homePages.BIM');
-});
-Route::get('/login', function () {
-    return view('homePages.auth.login');
-});
+Route::get('/classeDetail/{id}/{category_id}', [HomeController::class, 'showClasseDetails'])->name('class.detail');
+Route::get('/blogDetail/{id}', [HomeController::class, 'showBlogDetails'])->name('blog.detail');
+
+Route::post('/ajaxupload', [CommentController::class, 'upload'])->name('add.comment');
+Route::get('/comments/{id}', [CommentController::class, 'fetchComments']);
+
+Route::get('/classes', [HomeController::class, 'showClasses'])->name('show.classes');
+Route::get('/Blogs/{id}', [HomeController::class, 'showBlogs'])->name('show.blogs');
+Route::get('/MyBlogs/{id}', [HomeController::class, 'MyBlogs'])->name('show.myBlogs');
+Route::get('/coaches', [HomeController::class, 'showCoaches'])->name('show.coaches');
+Route::get('/bim', [HomeController::class, 'showBim'])->name('show.BIM');
+
+Route::post('/MyBlogs', [BlogsController::class, 'store'])->name('store.blogs');
 
 Route::get('/register',[RegisterController::class , 'index'])->name('show.register');
 Route::post('/register', [RegisterController::class, 'store'])->name('store.register');
@@ -43,11 +54,28 @@ Route::post('/login', [LoginController::class, 'login'])->name('check.login');
 
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
+Route::get('/resetPassword', [ResetPasswordController::class, 'index'])->name('show.resetPassword');
+Route::get('/forgetPassword', [ForgetPasswrodController::class, 'index'])->name('show.forgetPassword');
 
-Route::get('/forgetPassword', function () {
-    return view('homePages.auth.forgetPassword');
-});
-Route::get('/resetPassword', function () {
-    return view('homePages.auth.resetPassword');
-});
+Route::get('/staticDash', [DashboardController::class, 'showStaticSection'])->name('show.static');
+Route::get('/profileDash', [DashboardController::class, 'showProfilSection'])->name('show.profile');
+Route::get('/tableDash', [DashboardController::class, 'showTableSection'])->name('show.profile');
+Route::get('/classeDash', [DashboardController::class, 'showClasseSection'])->name('show.classes');
+Route::get('/categoryDash', [DashboardController::class, 'showCategoryTable'])->name('show.categories');
+Route::get('/userDash', [DashboardController::class, 'showUserTable'])->name('show.users');
+Route::get('/benefitDash', [DashboardController::class, 'showBenefitTable'])->name('show.benefits');
+Route::get('/featureDash', [DashboardController::class, 'showFeatureTable'])->name('show.features');
+
+Route::post('/class/add', [ClasseController::class, 'store'])->name('store.class');
+Route::post('/category/add', [CategoryController::class, 'store'])->name('store.category');
+Route::post('/benefit/add', [BenefitsController::class, 'store'])->name('store.benefit');
+
+Route::put('/updateClass/{id}', [ClasseController::class, 'updateClasse'])->name('update.classe');
+Route::put('/updateCategory/{id}', [CategoryController::class, 'updateCategory'])->name('update.category');
+Route::put('/updateBenefit/{id}', [BenefitsController::class, 'updateBenefit'])->name('update.benefit');
+
+Route::delete('/deleteClass/{id}', [ClasseController::class, 'deleteClasse'])->name('delete.classe');
+Route::delete('/deleteCategory/{id}', [CategoryController::class, 'deleteCategory'])->name('delete.category');
+Route::delete('/deleteBenefit/{id}', [BenefitsController::class, 'deleteBenefit'])->name('delete.benefit');
+Route::delete('/deleteBlog/{id}', [BlogsController::class, 'deleteBlog'])->name('delete.blog');
 
